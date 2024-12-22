056594f81fa04e51f33d77854f40dc913ccfc83c
Chesler John  Hamili <hamilicheslerjohn@gmail.com>
2024-12-22 13:19:29 +0800

    Made Podium

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:testonly/Leaderboard.dart';
+import 'package:testonly/Leaderboard_2.dart';
 import 'colors.dart';
 import 'qr_verse_logo.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+  List<User_stats> lists = [
+    User_stats(userName: 'Test-1', points: 0),
+    User_stats(userName: 'Test-2', points: 0),
+    User_stats(userName: 'Test-3', points: 0),
+    User_stats(userName: 'Test-4', points: 0)
+  ];
+
+  Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +24,19 @@ class Homepage extends StatelessWidget {
         ),
         toolbarHeight: 200,
       ),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'Leaderboard',
+              style: TextStyle(
+                  fontSize: 40, fontFamily: 'Poppins', color: Colors.white),
+            ),
+          ),
+          Leaderboard(list: lists),
+        ],
+      ),
+      backgroundColor: Colors.black,
     );
   }
 }
